618796bf8c2f91b96c0465367cbafe06aa395cc7
qwr14 <os_alena@yahoo.com>
2025-11-05 11:39:31 +0300

    next

--- lab1/vars.php
+++ lab1/vars.php
@@ -1,23 +1,26 @@
 <?php
-$name = 'Алиса';
-$age = 19;
+$name = 'Алёна`';
+$age = 21;
 ?>
 <!DOCTYPE html>
 <html lang="ru">
+
 <head>
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Переменные и вывод</title>
 </head>
+
 <body>
 	<h1>Переменные и вывод</h1>
-	<?php
-	echo "Меня зовут: $name<br>";
-	echo "Мне $age лет<br>";
-	echo "Тип переменной \$name: " . gettype($name) . "<br>";
-	echo "Тип переменной \$age: " . gettype($age) . "<br>";
-	unset($name, $age);
-	?> 
+	Меня зовут: <?= $name ?> <br>
+	Мне <?= $age ?> лет<br>
+	Тип переменной $name: <?= gettype($name) ?> <br>
+	Тип переменной $age: <?= gettype($age) ?> <br>
 </body>
+
 </html>
+<?php
+unset($name, $age);
+?>
