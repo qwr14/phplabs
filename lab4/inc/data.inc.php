8430fe47a08ce7b7ec77036a1f888854f72b93a2
qwr14 <os_alena@yahoo.com>
2025-12-17 15:55:51 +0300

    site

--- lab4/inc/data.inc.php
+++ lab4/inc/data.inc.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 $cols = $_GET['cols'] ?? 5;
 $rows = $_GET['rows'] ?? 7;
-$color =  $_SESSION['color'] ??  'lightgreen';
+$color =  $_SESSION['color'] ??  '#00ff00';
 
 $menu = [
     ['href' => 'index.php', 'link' => 'Домой'],
