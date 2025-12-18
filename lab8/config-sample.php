7513ce2631fbc409d79c4a157ff3862ee5dfeb02
qwr14 <os_alena@yahoo.com>
2025-12-18 13:12:45 +0300

    bd

---
+++ lab8/config-sample.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+
+const DB_HOST = 'localhost';
+const DB_USER = 'login_user';
+const DB_PASSWORD = 'your_password';
+const DB_NAME = 'login_db';
+const DB_CHARSET = 'utf8mb4';
+?>
