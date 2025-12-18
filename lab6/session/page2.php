964f4de125ab391c64a2826341e9910a9c015e50
qwr14 <os_alena@yahoo.com>
2025-12-18 13:29:24 +0300

    lab6

--- lab6/session/page2.php
+++ lab6/session/page2.php
@@ -1,7 +1,10 @@
 <?php
 declare(strict_types=1);
 
+ini_set("session.use_only_cookies", "0");
+ini_set("session.use_trans_sid", "1");
 
+// Открываем сессию
 session_start();
 
 // Подключаем код для сохранения информации о странице в сессии
